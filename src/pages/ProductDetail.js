88e7759661753b1f19f20e90f3cd9f3ed246849d
dhohirpradana <dhohirkudus@gmail.com>
2022-03-30 07:56:44 +0700

    5

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -8,26 +8,30 @@ import NotFound from "./NotFound";
 
 export default function ProductDetail() {
   const params = useParams();
-  var projects = JSON.parse(localStorage.getItem("products"));
-  var project = projects?.find((x) => x.id == params.id);
-  return projects ? (
+  var products = JSON.parse(localStorage.getItem("products"));
+  var product = products?.find((x) => x.id == params.id);
+  return product ? (
     <div>
       <NavBar />
       <div className="mx-5 px-5">
         <div className="d-flex mx-4 justify-content-center align-items-start">
+            
+          {/* Right Side */}
           <div className="me-4">
             <img
-              src={project.src}
-              alt={project.id}
+              src={product.src}
+              alt={product.id}
               style={{ height: "450px", width: "350px", objectFit: "cover" }}
             ></img>
           </div>
+
+          {/* Right Side */}
           <div className="ms-3 text-light fw-light mt-4">
-            <h3 className="primary-color-text">{project.name}</h3>
-            <p>Stock : {project.stock}</p>
-            <p id="p_wrap">{project.description}</p>
+            <h3 className="primary-color-text">{product.name}</h3>
+            <p>Stock : {product.stock}</p>
+            <p id="p_wrap">{product.description}</p>
             <div className="d-flex flex-row-reverse mb-4 mt-4">
-              <h5 className="primary-color-text">Rp.{project.price}</h5>
+              <h5 className="primary-color-text">Rp.{product.price}</h5>
             </div>
             <MDBBtn
               className="primary-color text-capitalize"
