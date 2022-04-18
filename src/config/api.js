467714b5b3ef7b937d817c2857fb81fe5739dce8
dhohirpradana <dhohirkudus@gmail.com>
2022-04-18 21:18:00 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: 'http://localhost:5000/api/v1/',
+  baseURL: 'https://dumbmerch.dhohirpradana.com/api/v1/',
 });
 
 export const setAuthToken = (token) => {
